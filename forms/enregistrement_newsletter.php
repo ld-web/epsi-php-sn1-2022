9175102ecd0bfac41241516179e00efc1bb96d13
Lucas LD-WEB <lucas@ld-web.net>
2022-10-10 16:20:09 +0200

    :sparkles: form intro (get method)

--- forms/enregistrement_newsletter.php
+++ forms/enregistrement_newsletter.php
@@ -9,5 +9,11 @@ if (isset($_GET['userEmail'])) {
   // Par exemple, enregistrement de l'email dans une
   // base de données
   // Pour l'instant, on ne va que l'afficher à l'écran
-  var_dump($_GET['userEmail']);
+  echo "Merci, " . $_GET['userEmail'] . "<br />";
+}
+
+if (isset($_GET['dataShareConsent'])) {
+  echo "Vous avez accepté le partage de vos données.";
+} else {
+  echo "Vous avez refusé le partage de vos données.";
 }
--- forms/form.php
+++ forms/form.php
@@ -5,14 +5,33 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css" />
   <title>Formulaire</title>
 </head>
 
 <body>
   <h1>Formulaire d'inscription à la newsletter</h1>
   <form action="enregistrement_newsletter.php">
-    <input type="email" name="userEmail" />
-    <input type="submit" value="Inscription" />
+    <div>
+      <input type="email" name="userEmail" placeholder="Email..." />
+    </div>
+    <div>
+      <!--
+      <input type="checkbox" name="dataShareConsent" id="dataShareConsent" value="yes" />
+      <label for="dataShareConsent">J'accepte le partage de mes données</label>
+      -->
+      <span>J'accepte le partage de mes données</span>
+      <input type="radio" name="shareConsent" value="yes" id="dataShareYes" />
+      <label for="dataShareYes">Oui</label>
+      <input type="radio" name="shareConsent" value="no" id="dataShareNo" />
+      <label for="dataShareNo">Non</label>
+    </div>
+    <div>
+      <input type="submit" value="Inscription" />
+    </div>
   </form>
 </body>
 
